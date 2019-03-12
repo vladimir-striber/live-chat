4ce6fceb2d42ac7fdb8fa41d286fa9f947bd6a33
karldzonson <stribervladimir@gmail.com>
2019-03-12 05:29:23 +0100

    Initial commit.

---
+++ src/firebase/init.js
@@ -0,0 +1,16 @@
+import firebase from "firebase"
+import firestore from "firebase/firestore"
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCgVueWwtQpOCYkE1W5_1P4qjkN9-eSbUo",
+    authDomain: "live-chat-835c2.firebaseapp.com",
+    databaseURL: "https://live-chat-835c2.firebaseio.com",
+    projectId: "live-chat-835c2",
+    storageBucket: "live-chat-835c2.appspot.com",
+    messagingSenderId: "1478454736"
+};
+const firebaseApp = firebase.initializeApp(config);
+firebaseApp.firestore().settings({ timestampsInSnapshots: true })
+
+export default firebaseApp.firestore()
